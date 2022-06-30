af8b84c4454397ef7de8f5882b4dd424740322d9
Gabriela Godoy Marques <gabyzinha.ggm@gmail.com>
2022-06-30 09:51:36 -0300

    shared atualizado

--- SharedPreference/app/src/main/java/br/com/zup/sharedpreference/MainActivity.kt
+++ SharedPreference/app/src/main/java/br/com/zup/sharedpreference/MainActivity.kt
@@ -2,16 +2,14 @@ package br.com.zup.sharedpreference
 
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.core.content.edit
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import br.com.zup.sharedpreference.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val pref = getSharedPreferences("Shared_preference", Context.MODE_PRIVATE)
-    private val prefEditor = pref.edit()
+    lateinit var pref: SharedPreferences
+    lateinit var prefEditor: SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,21 +18,41 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        iniciarPrefs()
+
+        recuperarPrefs()
+
+        binding.btnSalvar.setOnClickListener {
+            salvarPrefs()
+        }
+
+    }
+
+    private fun iniciarPrefs() {
+        pref = getSharedPreferences("Shared", Context.MODE_PRIVATE)
+        prefEditor = pref.edit()
+
+    }
+
+    private fun recuperarPrefs() {
+
         val nome = pref.getString("NOMEPRODUTO", "")
         val quantidade = pref.getInt("QUANTIDADEPRODUTO", 0)
 
         binding.etNomeProduto.setText(nome)
         binding.etQuantidadeProduto.setText(quantidade)
 
-        binding.btnSalvar.setOnClickListener {
+    }
 
-            val nomeProduto = binding.etNomeProduto.text.toString()
-            val quantidadeProduto = binding.etQuantidadeProduto.text.toString().toInt()
-            prefEditor.putString("NOMEPRODUTO", nomeProduto)
-            prefEditor.putInt("QUANTIDADEPRODUTO", quantidadeProduto)
-            prefEditor.apply()
+    private fun salvarPrefs() {
 
-        }
+        val nomeProduto = binding.etNomeProduto.text.toString()
+        val quantidadeProduto = binding.etQuantidadeProduto.text.toString().toInt()
+        prefEditor.putString("NOMEPRODUTO", nomeProduto)
+        prefEditor.putInt("QUANTIDADEPRODUTO", quantidadeProduto)
+        prefEditor.apply()
 
     }
+
 }
+
