6145ee8043f17c36302adf5c94f6761793b12ad6
Gabriela Godoy Marques <gabyzinha.ggm@gmail.com>
2022-07-05 18:05:49 -0300

    MVVM

--- Arquitetura/MovieFlix/app/src/main/java/br/com/zup/movieflix/register/repository/RegisterRepository.kt
+++ Arquitetura/MovieFlix/app/src/main/java/br/com/zup/movieflix/register/repository/RegisterRepository.kt
@@ -1,4 +1,13 @@
 package br.com.zup.movieflix.register.repository
 
+import br.com.zup.movieflix.register.model.RegisterModel
+
 class RegisterRepository {
+
+    private var user: RegisterModel = RegisterModel("", "")
+
+    fun registerUser(user: RegisterModel): RegisterModel {
+        this.user = user
+        return user
+    }
 }
--- Arquitetura/MovieFlix/app/src/main/java/br/com/zup/movieflix/register/viewmodel/RegisterViewModel.kt
+++ Arquitetura/MovieFlix/app/src/main/java/br/com/zup/movieflix/register/viewmodel/RegisterViewModel.kt
@@ -1,6 +1,23 @@
 package br.com.zup.movieflix.register.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.zup.movieflix.register.model.RegisterModel
+import br.com.zup.movieflix.register.repository.RegisterRepository
 
-class RegisterViewModel() : ViewModel() {
+class RegisterViewModel : ViewModel() {
+
+    private val repository: RegisterRepository = RegisterRepository()
+
+    private var _response: MutableLiveData<RegisterModel> = MutableLiveData()
+    val response: LiveData<RegisterModel> = _response
+
+    fun registerUser(user: RegisterModel) {
+        try {
+            _response.value = repository.registerUser(user)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
