0f3a96e7dea51ca65257132dc35dd96b126b538d
Gabriela Godoy Marques <gabyzinha.ggm@gmail.com>
2022-08-02 11:51:40 -0300

    exercicio 70

---
+++ Cadastro/app/src/main/java/br/com/zup/cadastro/ui/home/view/HomeActivity.kt
@@ -0,0 +1,10 @@
+package br.com.zup.cadastro.ui.home.view
+
+import android.app.Activity
+import android.view.inputmethod.InputBinding
+
+class HomeActivity App {
+    private lateinit var binding: ActivityHomeBinding
+
+
+}
